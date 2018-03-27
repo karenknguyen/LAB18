858902254868f8f75eede690e65aaf6d84ac27f7
Karen <35709976+karenknguyen@users.noreply.github.com>
2018-03-27 17:07:52 -0400

    lab midterm
    My 2018 Spring Core Lab Midterm of the SENSE(LET).

---
+++ public/main.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 8080;
+var SerialPort = require('serialport');
+var serialVal;
+
+const Readline = SerialPort.parsers.Readline; //how to read raw data
+//add your path to the serialport
+const mySerialPort = new SerialPort('/dev/cu.usbmodem1441' , {baudRate:9600}); 
+const parser = mySerialPort.pipe(new Readline({ delimiter: '\n' })); //try adding different delimiter;
+//the delimiter is where your parser will split your data 
+
+//show to see raw bytes
+//mySerialPort.on('data', console.log); 
+
+parser.on('data', dataToSerialVal); 
+
+
+function dataToSerialVal(data){
+ 	serialVal= data.toString();
+ 	console.log(serialVal);
+ }
+
+
+server.listen(port, function(){
+	console.log('Server listening on ' + port);
+});
+
+io.on('connection',function(client){
+	console.log('Socket connected...');
+	client.emit('messages', {serialValue: serialVal});//inital value
+	client.on('getSerialVal', function(){
+		client.emit('messages', {serialValue: serialVal});
+
+	});
+
+});
+app.use(express.static("."));
+
+app.get('/', function(req,res){
+	console.log('serving index.html');
+	res.sendFile(__dirname + '/digitalRead.html');
+
+});
